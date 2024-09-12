90d45530ae653eb60983c93d6c932c4e16898d8a
Mike <mpullen9@gmail.com>
2024-09-12 21:20:06 +0100

    Improve overview scene with vologram, better model and splines (#14)
    - Added the first (and probably only) Vologram to the Overview Scene
    - Made improvements to the hot air balloon allowing better viewing of
    the surroundings (only for Overview Scene)
    - Disabled manual movement of the hot air balloon and replaced it with
    spline-based movement.

--- Assets/Scripts/TransitionManager.cs
+++ Assets/Scripts/TransitionManager.cs
@@ -8,6 +8,14 @@ public class TransitionManager : MonoBehaviour
     private Globals globals;
     [SerializeField]
     private bool SkipTransition = false;
+    [SerializeField]
+    private float Delay = 1f;
+    [SerializeField]
+    private float Duration = 1f;
+    [SerializeField]
+    private float FromValue = 0f;
+    [SerializeField]
+    private float ToValue = 1f;
 
     private void OnEnable()
     {
@@ -39,24 +47,24 @@ public class TransitionManager : MonoBehaviour
         }
         else
         {
-            StartCoroutine(PlayAnimation(0f, 1f));
+            StartCoroutine(PlayAnimation(this.FromValue, this.ToValue));
         }
     }
 
     IEnumerator PlayAnimation(float initial, float target)
     {
-        yield return new WaitForSeconds(1f); // Wait for 1 second before starting the reveal
+        yield return new WaitForSeconds(this.Delay); // Wait for 1 second before starting the reveal
 
         float t = 0f;
         MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
 
-        // Initial value of the dissolve amount (you can adjust this as needed)
+        // Initial value of the dissolve amount
         float initialDissolveAmount = initial; // Assuming the effect starts fully "dissolved"
         float targetDissolveAmount = target; // Target value to fully "reveal" the object
 
         while (t < 1f)
         {
-            t += Time.deltaTime / 1f; // Adjust the divisor to change the speed of the transition
+            t += Time.deltaTime / this.Duration; // Adjust the divisor to change the speed of the transition
             float dissolveAmount = Mathf.Lerp(initialDissolveAmount, targetDissolveAmount, t);
             meshRenderer.material.SetFloat("_Dissolve", dissolveAmount);
             yield return null; // Wait until the next frame
