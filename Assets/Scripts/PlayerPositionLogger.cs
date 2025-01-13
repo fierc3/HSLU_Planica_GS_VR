667097e4a1ffa9c20c26929383b6ca40bb8bcff8
Mike <mpullen9@gmail.com>
2025-01-13 11:02:24 +0100

    Add tutorial and logging (#23)
    Improved tutorial and logging. Final PR for V1.1.0. Also improved some
    other minor things at that were "quick wins".
     
    Patch Notes:
    - Rename Project for better pathing in AppData
    - Improve tutorial which includes teleporting.
    - Added logging (tutorial scene excluded)
    - Added shortcuts to make testing easier
    - Disabled origin tracking (yielded better results but has drawback that short people might have more problems seeing over fences)
    
    ---------
    
    Co-authored-by: Mike <>

---
+++ Assets/Scripts/PlayerPositionLogger.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PlayerPositionLogger : MonoBehaviour
+{
+    private GameObject player;
+    private string logFilePath;
+    private List<string> logEntries = new List<string>();
+    private float writeToDiskInterval = 10f; // Log every 10 seconds
+    private float logInterval = 0.2f;
+    private float logIntervalIndex = 0f;
+    private float writeTimer;
+    private float logTimer;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        string timestamp = System.DateTime.Now.ToString("yyyyMMdd_HHmmssfff");
+        logFilePath = Path.Combine(Application.persistentDataPath, $"{timestamp}.csv");
+        if (!File.Exists(logFilePath))
+        {
+            File.Create(logFilePath).Dispose(); // Ensure the file is created and closed
+        }
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+        FindPlayerHead();
+        AddLogEntry(player.transform.position, "Experience started");
+    }
+
+    void Update()
+    {
+        writeTimer += Time.deltaTime;
+        logTimer += Time.deltaTime;
+
+        if (player != null)
+        {
+            Vector3 currentPosition = player.transform.position;
+            if (logTimer > logInterval)
+            {
+                AddLogEntry(currentPosition);
+                logTimer = 0f;
+            }
+        }
+
+        if (writeTimer >= writeToDiskInterval)
+        {
+            WriteLogEntries();
+            writeTimer = 0f;
+        }
+    }
+
+    void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode mode)
+    {
+        FindPlayerHead();
+    }
+
+    void FindPlayerHead()
+    {
+        player = GameObject.FindWithTag("MainCamera");
+    }
+
+    void AddLogEntry(Vector3 position, string comment = "")
+    {
+        logIntervalIndex += logTimer;
+
+        string logEntry = $"{logIntervalIndex},{position.x},{position.z},{UnityEngine.SceneManagement.SceneManager.GetActiveScene().name},{comment}";
+        logEntries.Add(logEntry);
+    }
+
+    void WriteLogEntries()
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                foreach (string logEntry in logEntries)
+                {
+                    writer.WriteLine(logEntry);
+                }
+            }
+            logEntries.Clear();
+        }
+        catch (IOException ex)
+        {
+            Debug.LogError("Failed to write to log file: " + ex.Message);
+        }
+    }
+
+    void OnDestroy()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        AddLogEntry(Vector3.zero, "Experience ended");
+        WriteLogEntries(); // Ensure remaining entries are written on destroy
+    }
+}
+
---
+++ Assets/Scripts/ViewResetter.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ViewResetter : MonoBehaviour
+{
+    public XROrigin xrOrigin;
+    public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //xrOrigin = FindAnyObjectByType<XROrigin>();
+        startPosition = xrOrigin.transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) // Change the key as needed 
+        {
+            Debug.Log("Resetting");
+            //StartCoroutine(Reset());
+            ResetSubsystem();
+            xrOrigin.transform.localPosition = startPosition;
+            /*
+            InputTracking.Recenter();
+            */
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.X)) // Change the key as needed 
+        {
+            Debug.Log("Restarting");
+            System.Diagnostics.Process.Start(Application.dataPath.Replace("_Data", ".exe")); // Start a new instance of the application
+            Application.Quit(); // Quit the current instance
+
+        }
+
+    }
+
+
+    void ResetSubsystem()
+    {
+        var inputSubsystems = new List<XRInputSubsystem>();
+        SubsystemManager.GetInstances(inputSubsystems);
+        Debug.Log("Subsystems: " + inputSubsystems.Count);
+
+        foreach (var subsystem in inputSubsystems)
+        {
+            Debug.Log(subsystem.ToString());
+            if (subsystem.TryRecenter())
+            {
+                Debug.Log("View recentered successfully.");
+            }
+            else
+            {
+                Debug.LogWarning("Failed to recenter view.");
+            }
+        }
+    }
+
+    IEnumerator Reset()
+    {
+        xrOrigin.transform.localPosition = startPosition;
+        yield return new WaitForSeconds(0.05f);
+        xrOrigin.RequestedTrackingOriginMode = XROrigin.TrackingOriginMode.Floor;
+        yield return new WaitForSeconds(0.1f);
+        xrOrigin.RequestedTrackingOriginMode = XROrigin.TrackingOriginMode.Device;
+    }
+}
